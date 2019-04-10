0a3864b613b1cd43f0faa5d22e292487aa34fda9
Abdullahi Musa <abdullahimusa4991@gmail.com>
2019-04-10 08:15:19 +0300

    removing the websites from the block list while at the fun hours

--- website_blocker.py
+++ website_blocker.py
@@ -1,16 +1,16 @@
 import time
 from datetime import datetime as dt
 
-# hosts_path=r"etc/hosts"
+hosts_path=r"/etc/hosts"
 # hosts_path=r"C:\windows\system32\drivers\etc\hosts"
-hosts_temp="hosts"
+# hosts_path="hosts"
 redirect="127.0.0.1"
 website_list=['www.facebook.com','facebook.com']
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours......")
-        with open(hosts_temp,'r+') as file:
+        with open(hosts_path,'r+') as file:
             content=file.read()
             for website in website_list:
                 if website in content:
@@ -18,5 +18,12 @@ while True:
                 else:
                     file.write(redirect+" "+website+"\n")
     else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours....")
     time.sleep(5)
